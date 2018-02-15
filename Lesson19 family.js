7173dd95750e74bd3b6fd5699f77478c6c4d57f7
QAMinn2468 <33698407+QAMinn2468@users.noreply.github.com>
2018-02-15 14:16:12 -0500

    Solved!
    :)

--- Lesson19 family.js
+++ Lesson19 family.js
@@ -4,27 +4,25 @@ For this quiz, you'll need to add to the DOM tree that already exists.
 '#family2' should be a sibling of and come after '#family1'. '#bruce' should be the only immediate child
 of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hunter'.
 */
-
+var family1;
 var family2;// = $('#family2');
 var bruce;//=$('#bruce');
 var madison;  //=$('#madison');
 var hunter; //=$('#hunter');
 
 
+family1 = $('#family1');
+family2 = $('<div id="family2"><h1>Family2</h1></div>');
 
+bruce = $('<div id="bruce"><h2>Bruce</h2></div>');
 
-$('family2').html(<div id='family2'><h1>Family2</h1></div>');
+madison = $('<div id="madison"><h3>Madison</h3></div>');
 
-$('bruce').html(<div id='bruce'><h2>bruce</h2></div>);
-
-$('madison').html(<div id='madison'><h3>madison</h3></div>);
-
-$('hunter').html(<div id='hunter'><h3>hunter</h3></div>);
-
+hunter = $('<div id="hunter"><h3>Hunter</h3></div>');
 
 
 
-$('#family1').insertAfter('family2');// #family2 is inserted after family1
-$('#family2').prepend('bruce');  // #bruce is added as a child of family2
-$('#bruce').prepend('madison');  //adds #madison as a child of #bruce
-$('#bruce').append('hunter');  //  adds #hunter as the second child of #bruce
+family2.insertAfter(family1);// #family2 is inserted after family1
+family2.append(bruce);  // #bruce is added as a child of family2
+bruce.append(madison);  //adds #madison as a child of #bruce
+bruce.append(hunter);  //  adds #hunter as the second child of #bruce
